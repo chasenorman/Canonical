5b07d8d4e6eb0ac56c818bd26835ac1e4db3f376
Chase Norman <c_@berkeley.edu>
2025-05-03 10:14:52 -0400

    change dynlib to canonical_lean

--- build_lean.py
+++ build_lean.py
@@ -5,6 +5,7 @@ import os
 import platform
 
 SYSTEM = platform.system()
+LIB_NAME = "canonical_lean"
 
 def name_to_shared_lib(name):
     if SYSTEM == "Windows":
@@ -14,18 +15,18 @@ def name_to_shared_lib(name):
     else:
         return f"lib{name}.so"
 
-LEAN_LIB = os.path.join("lean", ".lake", "packages", "Canonical", ".lake", "build", "lib", name_to_shared_lib("canonical"))
+LEAN_LIB = os.path.join("lean", ".lake", "packages", "Canonical", ".lake", "build", "lib", name_to_shared_lib(LIB_NAME))
 
 if SYSTEM == "Windows":
-    TARGET = os.path.join("target", "x86_64-pc-windows-gnu", "release", name_to_shared_lib("canonical"))
+    TARGET = os.path.join("target", "x86_64-pc-windows-gnu", "release", name_to_shared_lib(LIB_NAME))
 else:
-    TARGET = os.path.join("target", "release", name_to_shared_lib("canonical"))
+    TARGET = os.path.join("target", "release", name_to_shared_lib(LIB_NAME))
 
 def main():
     if SYSTEM == "Windows":
-        subprocess.run(["cargo", "build", "-p", "canonical", "--release", "--target", "x86_64-pc-windows-gnu"], shell=False, check=True)
+        subprocess.run(["cargo", "build", "-p", LIB_NAME, "--release", "--target", "x86_64-pc-windows-gnu"], shell=False, check=True)
     else:
-        subprocess.run(["cargo", "build", "-p", "canonical", "--release"], shell=False, check=True)
+        subprocess.run(["cargo", "build", "-p", LIB_NAME, "--release"], shell=False, check=True)
     shutil.copy2(TARGET, LEAN_LIB)
     
 if __name__ == "__main__":
